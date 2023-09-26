6429d9cb88ff52e1581fdc74a332ca0f2a0f3094
Your Name <you@example.com>
2023-09-26 09:36:40 +0700

    ganti boss

--- lib/main.dart
+++ lib/main.dart
@@ -149,7 +149,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Patuhfy',
+      title: 'Patuhfy V2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
